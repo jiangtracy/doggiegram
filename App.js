f65988dda2d5b114f43c13290bf76b7ebf9f67a8
Tracy Jiang <tracyjiangmx@gmail.com>
2021-02-28 23:48:56 -0800

    refactor and clean up App.js

--- App.js
+++ App.js
@@ -1,57 +1,13 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
-import HomeScreen from './src/screens/HomeScreen/';
-import DiscoveryScreen from './src/screens/DiscoveryScreen/';
-import CreatePostScreen from './src/screens/CreatePostScreen/';
-import NotificationScreen from './src/screens/NotificationScreen/';
-import ProfileScreen from './src/screens/ProfileScreen/';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-import { Foundation } from '@expo/vector-icons';
-import { Feather } from '@expo/vector-icons';
-import { AntDesign } from '@expo/vector-icons'; 
-import { Ionicons } from '@expo/vector-icons';
-
-const Tab = createBottomTabNavigator();
+import Router from './src/components/Router';
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <StatusBar barStyle="dark-content" />
-
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            if (route.name === 'Home') {
-              return <Foundation name="home" size={24} color="black" />
-            } 
-            if (route.name === 'Discovery') {
-              return <Feather name="search" size={24} color="black" />
-            } 
-            if (route.name === 'Post') {
-              return <Feather name="plus-square" size={24} color="black" />
-            } 
-            if (route.name === 'Notification') {
-              return    <AntDesign name="hearto" size={24} color="#4f4f4f" />
-            } 
-            if (route.name === 'Profile') {
-              return <Ionicons name="person-outline" size={24} color="black" />
-            } 
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: 'tomato',
-          inactiveTintColor: 'gray',
-          showLabel: false,
-        }}
-      >
-        <Tab.Screen name="Home" component={HomeScreen}/>
-        <Tab.Screen name="Discovery" component={DiscoveryScreen} />
-        <Tab.Screen name="Post" component={CreatePostScreen} />
-        <Tab.Screen name="Notification" component={NotificationScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<StatusBar barStyle="dark-content" />
+			<Router />
+		</NavigationContainer>
+	);
 }
---
+++ src/components/HomeRoutes.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, Image } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen/';
+import { Feather } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import logo from '../assets/images/logo.png';
+
+const HomeStack = createStackNavigator();
+
+const HomeRoutes = () => (
+	<HomeStack.Navigator>
+		<HomeStack.Screen
+			name="Home"
+			component={HomeScreen}
+			options={{
+				title: 'Instagram',
+				headerLeft: () => (
+					<View style={{ marginLeft: 10 }}>
+						<Feather name="camera" size={24} color="black" />
+					</View>
+				),
+				headerRight: () => (
+					<View style={{ marginRight: 10 }}>
+						<Ionicons name="paper-plane-outline" size={25} />
+					</View>
+				),
+				headerTitle: () => <Image source={logo} resizeMode="contain" style={{ width: 100 }} />
+			}}
+		/>
+	</HomeStack.Navigator>
+);
+
+export default HomeRoutes;
