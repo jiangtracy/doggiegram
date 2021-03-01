069f782e9f07dd674d671a6b6b63acd7499c067f
Tracy Jiang <tracyjiangmx@gmail.com>
2021-02-28 16:45:46 -0800

    Complete adding icons for post

--- src/components/Post/Footer.js
+++ src/components/Post/Footer.js
@@ -1,25 +1,45 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { AntDesign } from '@expo/vector-icons'; 
 import { EvilIcons } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
 import { FontAwesome } from '@expo/vector-icons';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 
-const Footer = ({likesCount, caption, postedAt}) => (
-  <View style={styles.container} >
-    <View style={styles.icons} >
-      <View style={styles.left} >
-        <AntDesign name="hearto" size={24} color="#4f4f4f" />
-        <EvilIcons name="comment" size={32} color="#4f4f4f" />
-        <Ionicons name="paper-plane-outline" size={25} color="#4f4f4f" />
+const Footer = ({likesCount : propsLikeCounts, caption, postedAt}) => {
+  const [isLiked, setIsLinked] = useState(false);
+  const [likeCount, setLikeCount] = useState(0);
+
+  const onLike = () => {
+    const amount = isLiked ? -1 : 1;
+    setLikeCount(likeCount + amount);
+    setIsLinked(!isLiked);
+  }
+
+  useEffect(() => {
+    setLikeCount(propsLikeCounts);
+  }, [])
+
+  return (
+    <View style={styles.container} >
+      <View style={styles.icons} >
+        <View style={styles.left} >
+          <TouchableWithoutFeedback onPress={onLike}>
+            {isLiked ? 
+            <AntDesign name="heart" size={24} color="#e73838" /> :
+            <AntDesign name="hearto" size={24} color="#4f4f4f" />
+            }
+          </TouchableWithoutFeedback>
+          <EvilIcons name="comment" size={32} color="#4f4f4f" />
+          <Ionicons name="paper-plane-outline" size={25} color="#4f4f4f" />
+        </View>
+        <FontAwesome name="bookmark-o" size={25} color="#4f4f4f" />
       </View>
-      <FontAwesome name="bookmark-o" size={25} color="#4f4f4f" />
+      <Text style={styles.likes}>{likeCount} Likes</Text>
+      <Text style={styles.caption}>{caption}</Text>
+      <Text style={styles.postedAt}>{postedAt}</Text>
     </View>
-    <Text style={styles.likes}>{likesCount} Likes</Text>
-    <Text style={styles.caption}>{caption}</Text>
-    <Text style={styles.postedAt}>{postedAt}</Text>
-  </View>
-)
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
